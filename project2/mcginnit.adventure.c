d5df0759beaf4572de0eb921a4edb3dfde3a2c6d
Tyler McGinnis <mcginnit@eos-class.engr.oregonstate.edu>
2016-04-22 13:42:39 -0700

    Continue to add Room functionality

--- project2/mcginnit.adventure.c
+++ project2/mcginnit.adventure.c
@@ -3,21 +3,25 @@
 #include <stdlib.h>
 
 
+#define MAX_CONNECTIONS 6
+
 struct Room {
     char* room_name;
-    struct Room *room_connections[6];
+    struct Room *room_connections[MAX_CONNECTIONS];
     char room_type;
 };
 
 struct Room *room_create(char* room_name, char room_type)
 {
     int i;
-    int arr_size;
     struct Room *new_room = malloc(sizeof(struct Room));
     new_room->room_name = room_name;
     new_room->room_type = room_type;
 
     // initialize room_connections to null
+    for (i = 0; i < MAX_CONNECTIONS; i++) {
+        new_room->room_connections[i] = NULL;
+    }
 
     return new_room;
 }
@@ -30,9 +34,21 @@ void room_destroy(struct Room *room)
 void add_connection(struct Room *room, struct Room *connection)
 {
     // check that each room has less than 6 connections first
+    // check that rooms aren't already connected
     // then add mutual connection between the two rooms
     printf("%i\n", sizeof(room));
     printf("%i\n", sizeof(room->room_connections));
+}
+
+int check_connection(struct Room *room, struct Room *connection)
+{
+    int i;
+    for (i = 0; i < MAX_CONNECTIONS; i++) {
+        if (room->room_connections[i] == connection) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 
@@ -48,7 +64,12 @@ int main(int argc, char *argv[])
     struct Room *room_ptr = first_room->room_connections[0];
     printf("%s\n", room_ptr->room_name);
 
-    add_connection(first_room, second_room);
+    int i;
+    for (i = 0; i < MAX_CONNECTIONS; i++) {
+        printf("%p\n", first_room->room_connections[i]);
+    }
+    int connected = check_connection(first_room, second_room);
+    printf("%i\n", connected);
 
     room_destroy(first_room);
     room_destroy(second_room);
